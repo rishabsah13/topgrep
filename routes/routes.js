5e5558b405a16f563a8368fbd77d262eea9958c7
Rishab Sah <you@example.com>
2023-01-02 21:41:02 +0530

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const router = express.Router();
+const Model = require("../model/model");
+
+router.post("/post", async (req, res) => {
+  const data = new Model({
+    name: req.body.name,
+  });
+
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+router.get("/getAll", async (req, res) => {
+  try {
+    const data = await Model.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
